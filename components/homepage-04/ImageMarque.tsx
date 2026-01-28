61ad48a3cc14e872862e81cb16632b8bb33643ea
Pranay Verma <pranayverma38@gmail.com>
2026-01-28 19:53:45 +0530

    ss

--- components/homepage-04/ImageMarque.tsx
+++ components/homepage-04/ImageMarque.tsx
@@ -4,28 +4,28 @@ import TextAppearAnimation from '../animation/TextAppearAnimation'
 
 const ImageMarque = () => {
   return (
-    <section className="overflow-hidden pb-14 pt-14 md:pb-16 md:pt-16 lg:pb-[100px] lg:pt-[100px] xl:pb-[130px] xl:pt-[130px]">
+    <section className="overflow-hidden pb-10 pt-10 md:pb-16 md:pt-16 lg:pb-[100px] lg:pt-[100px] xl:pb-[130px] xl:pt-[130px]">
           <TextAppearAnimation>
-            <h3 className="text-appear mb-8 text-center instrument-serif-regular text-[48px] md:text-[57px] lg:mb-[52px] lg:text-[64px] xl:text-[74px] xl:leading-[1.1]">
+            <h3 className="text-appear mb-6 text-center instrument-serif-regular text-[32px] sm:text-[40px] md:text-[57px] lg:mb-[52px] lg:text-[64px] xl:text-[74px] xl:leading-[1.1]">
               The <span className="italic">Sensory Room</span>
             </h3>
           </TextAppearAnimation>
       <RevealWrapper className="marquee-container">
         <Marquee pauseOnHover speed={40} autoFill>
-          <div className="flex items-end gap-3 first:mr-3">
-            <figure className="group relative min-w-96 md:max-w-[362px] h-[410px] md:h-[430px] overflow-hidden">
+          <div className="flex items-end gap-2 sm:gap-3 first:mr-3">
+            <figure className="group relative w-52 sm:w-64 md:min-w-96 md:max-w-[362px] h-64 sm:h-72 md:h-[430px] overflow-hidden">
               <img src="/images/11.jpg" alt=" 1" className="h-full w-full object-cover" />
             </figure>
-            <figure className="group relative min-w-96 md:max-w-[362px] h-[558px] md:h-[538px] overflow-hidden">
+            <figure className="group relative w-52 sm:w-64 md:min-w-96 md:max-w-[362px] h-72 sm:h-80 md:h-[538px] overflow-hidden">
               <img src="/images/22.jpg" alt=" 2" className="h-full w-full object-cover" />
             </figure>
-            <figure className="group relative min-w-96 md:max-w-[362px] h-[323px] md:h-[343px] overflow-hidden">
+            <figure className="group relative w-52 sm:w-64 md:min-w-96 md:max-w-[362px] h-56 sm:h-64 md:h-[343px] overflow-hidden">
               <img src="/images/33.jpg" alt=" 3" className="h-full w-full object-cover" />
             </figure>
-            <figure className="group relative min-w-96 md:max-w-[362px] h-[515px] md:h-[535px] overflow-hidden">
+            <figure className="group relative w-52 sm:w-64 md:min-w-96 md:max-w-[362px] h-72 sm:h-80 md:h-[535px] overflow-hidden">
               <img src="/images/44.jpg" alt=" 4" className="h-full w-full object-cover" />
             </figure>
-            <figure className="group relative min-w-96 md:max-w-[362px] h-[421px] md:h-[441px] overflow-hidden">
+            <figure className="group relative w-52 sm:w-64 md:min-w-96 md:max-w-[362px] h-60 sm:h-72 md:h-[441px] overflow-hidden">
               <img src="/images/55.jpg" alt=" 5" className="h-full w-full object-cover" />
             </figure>
           </div>
